74efca109da0ab73b700c6f06dab2c89fdd18213
soumya1103 <soumya.agrawal@nucleusteq.com>
2024-07-30 16:43:23 +0530

    Steps Updated

---
+++ src/Steps/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
--- src/Steps/Steps.jsx
+++ src/Steps/Steps.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./steps.css";
+import Button from "./Button";
+import StepMessage from "./StepMessage";
 
 const messages = [
   "Learn React ⚛️",
@@ -36,22 +38,14 @@ export default function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
+              <span>👈</span>Previous
+            </Button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
+              Next<span>👉</span>{" "}
+            </Button>
           </div>
         </div>
       ) : (
